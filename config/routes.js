e474c93182b76c16f266533abb44cfcdc3c0cfc5
Jay Ryoo <jay.ryoo@gmail.com>
2015-04-12 00:00:58 -0700

    para tu

--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,10 @@ module.exports = function (app) {
     app.get('/', function(req, res) {
         res.render('index.html');
     });
+    app.get('/boiler', function(req, res) {
+    	res.render('boiler.html');
+    });
+    app.get('/recognition', function(req, res) {
+    	res.render('recognition.html');
+    });
 };
